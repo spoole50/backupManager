b7331c0c6572c7665426a885842b753820ebb396
Shane Poole <shane.poole@ucsf.edu>
2021-09-26 20:43:41 -0700

    Walk OS tree for source folder

--- backupManager.py
+++ backupManager.py
@@ -7,13 +7,21 @@ def generateParse():
     parser.add_argument('SRC' ,
                         type=str,
                         help='Parent Folder to begin copy operation')
+    parser.add_argument('TARGET',
+                        type=str,
+                        help='Destination for copy operation')
     args = parser.parse_args()
     return args
 
 def main():
     args = generateParse()
     srcPath = os.path.abspath(args.SRC)
-    print(srcPath)
+    targetPath = os.path.abspath(args.TARGET)
+
+    for subdir, dirs, files in os.walk(srcPath):
+        for fle in files:
+            print(os.path.join(subdir, fle))
+            print(os.path.relpath(os.path.join(subdir, fle), srcPath))
 
 if __name__ == '__main__':
     main()
