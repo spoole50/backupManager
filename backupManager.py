f0100d3543879161be97bb4056360d4e55252607
Shane Poole <shane.poole@ucsf.edu>
2021-09-29 08:55:27 -0700

    Added summary report on completion or keyboard interupt

--- backupManager.py
+++ backupManager.py
@@ -3,6 +3,11 @@ import sys
 import os
 import shutil
 from zlib import crc32
+import pprint
+from timeit import default_timer as timer
+from datetime import timedelta
+
+_RunStats = {'start':0, 'end':0, 'totFiles':0, 'totSize':0, 'fileDict': {}}
 
 def generateParse():
     parser = argparse.ArgumentParser()
@@ -15,8 +20,16 @@ def generateParse():
     args = parser.parse_args()
     return args
 
-def genHash(fName):
+def sizeof_fmt(num, suffix="B"):
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+def genHash(fName, verbose=True):
     resHash = 0
+    fSize = os.path.getsize(fName)
     with open(fName, 'rb') as inFile:
         while True:
             read_data = inFile.read(25165824)
@@ -24,29 +37,55 @@ def genHash(fName):
                 break
             else:
                 resHash = crc32(read_data, resHash)
-    return resHash
+    if verbose:
+        print(f"{resHash} - {sizeof_fmt(fSize)} - {fName}")
+    return resHash, fSize
 
-def main():
-    fileDict = {}
+def processBackup():
     args = generateParse()
     srcPath = os.path.abspath(args.SRC)
     targetPath = os.path.abspath(args.TARGET)
 
     for subdir, dirs, files in os.walk(srcPath):
         for cFile in files:
+            
             srcFile = os.path.join(subdir, cFile)
             targetSubdir = os.path.join(targetPath, os.path.relpath(subdir, srcPath))
             # targetFile = os.path.join(targetPath, os.path.relpath(os.path.join(subdir, cFile), srcPath))
             targetFile = os.path.join(targetSubdir, cFile)
-            crc_hash = genHash(srcFile)
-            if crc_hash not in fileDict:
-                fileDict[crc_hash] = [srcFile]
+            crc_hash, tSize = genHash(srcFile)
+            _RunStats['totSize'] += tSize
+            _RunStats['totFiles'] += 1
+            if crc_hash not in _RunStats['fileDict']:
+                _RunStats['fileDict'][crc_hash] = [srcFile]
             else:
-                fileDict[crc_hash].append(srcFile)
+                _RunStats['fileDict'][crc_hash].append(srcFile)
             # os.makedirs(targetSubdir, exist_ok=True)
             # shutil.copy2(srcFullPath, targetFile)
-            # print(f"File: {crc_hash:x} - {srcFile}\n\tOutputSubdir: {targetSubdir}")
-    print(fileDict)
+            # print(f"File: {crc_hash:x} - {srcFile}\n\tOutputSubdir: {targetSubdir}")   
+
+def sumReport():
+    pp = pprint.PrettyPrinter()
+    _RunStats['end'] = timer()
+
+    print(f"""\n\nSummary Report:
+    Elapsed Time: {timedelta(seconds=_RunStats['end'] - _RunStats['start'])}
+    Files Scanned: {_RunStats['totFiles']}
+    Total Size: {sizeof_fmt(_RunStats['totSize'])}
+    \nFiles Indexed:\n""")
+
+    pp.pprint(_RunStats['fileDict'])
+
+def main():
+    _RunStats['start'] = timer()
+    try:
+        processBackup()
+    except KeyboardInterrupt:
+        sumReport()
+        sys.exit(0)
+    finally:
+        sumReport()
+        
 
 if __name__ == '__main__':
     main()
