9de4764a9640d5fa5f7f89459ff51f6a4e886025
Shane Poole <shane.poole@ucsf.edu>
2021-10-07 20:58:16 -0700

    Broke parsing into seperate function, added flags to state, made verbose look better with filename padding

--- backupManager.py
+++ backupManager.py
@@ -8,7 +8,14 @@ from timeit import default_timer as timer
 from datetime import timedelta
 import hashlib
 
-_RunStats = {'start':0, 'totFiles':0, 'totSize':0, 'fileDict': {}}
+_RunStats = {'start':0,
+            'totFiles':0,
+            'totSize':0,
+            'hashAlgo':'crc32',
+            'fileDict': {},
+            'flags':{
+                'move':0
+            }}
 
 def generateParse():
     parser = argparse.ArgumentParser()
@@ -18,17 +25,28 @@ def generateParse():
     parser.add_argument('TARGET',
                         type=str,
                         help='Destination for copy operation')
+    parser.add_argument('-a', '--algorithm',
+                        type=str,
+                        help='Algorithm to utilize for generation of file hashes')
     args = parser.parse_args()
     return args
 
+def parseArgs():
+    args = generateParse()
+    srcPath = os.path.abspath(args.SRC)
+    targetPath = os.path.abspath(args.TARGET)
+    if args.algorithm is not None:
+        _RunStats['hashAlgo'] = args.algorithm
+    return srcPath, targetPath
+
 def sizeof_fmt(num, suffix="B"):
-    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]: 
         if abs(num) < 1024.0:
             return f"{num:3.1f}{unit}{suffix}"
         num /= 1024.0
     return f"{num:.1f}Yi{suffix}"
 
-def genHash(fName, hashAlgo='crc32', blockChunk=128, verbose=None):
+def genHash(fName, hashAlgo, blockChunk=128, verbose=None):
     resHash = 0
     read_data = 1
     fSize = os.path.getsize(fName)
@@ -60,10 +78,7 @@ def getYN():
         return False
 
 def processBackup():
-    args = generateParse()
-    srcPath = os.path.abspath(args.SRC)
-    targetPath = os.path.abspath(args.TARGET)
-
+    srcPath, targetPath = parseArgs()
     for subdir, dirs, files in os.walk(srcPath):
         for cFile in files:
             try:
@@ -71,7 +86,7 @@ def processBackup():
                 targetSubdir = os.path.join(targetPath, os.path.relpath(subdir, srcPath))
                 # targetFile = os.path.join(targetPath, os.path.relpath(os.path.join(subdir, cFile), srcPath))
                 targetFile = os.path.join(targetSubdir, cFile)
-                crc_hash, tSize = genHash(srcFile, hashAlgo='md5', verbose=1)
+                crc_hash, tSize = genHash(srcFile, _RunStats['hashAlgo'], verbose=1)
                 if crc_hash not in _RunStats['fileDict']:
                     _RunStats['fileDict'][crc_hash] = [srcFile]
                 else:
@@ -80,7 +95,7 @@ def processBackup():
                 _RunStats['totFiles'] += 1
                 # os.makedirs(targetSubdir, exist_ok=True)
                 # shutil.copy2(srcFullPath, targetFile)
-                print(f"File: {crc_hash:x} - {srcFile}\n\tOutputSubdir: {targetSubdir}")
+                print(f"{'Source:': <13} {srcFile}\n{'OutputSubdir:': <13} {targetFile}\n")
             except OSError as oe:
                 print(f"\n{oe}\n\nContinue to next file? (Y/N):")
                 if getYN():
@@ -99,6 +114,7 @@ def sumReport(printDict=False):
     Files Scanned: {_RunStats['totFiles']}
     Total Size: {sizeof_fmt(_RunStats['totSize'])}
     Avg. Tranfer Speed: {sizeof_fmt(_RunStats['totSize']/totalTime.total_seconds())}/s
+    File Hash Algorithm: {_RunStats['hashAlgo']}
     """)
 
     if printDict:
@@ -109,6 +125,7 @@ def sumReport(printDict=False):
 def main():
     _RunStats['start'] = timer()
     try:
+        parseArgs()
         processBackup()
         sumReport()
     except OSError as e:
